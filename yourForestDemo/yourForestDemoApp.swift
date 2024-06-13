87f11338f3599dd9606bafcf440b093881016a63
Karsten Wennerlund <kandw15@gmail.com>
2024-06-13 13:35:01 -0500

    initial xcode project creation

---
+++ yourForestDemo/yourForestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  yourForestDemoApp.swift
+//  yourForestDemo
+//
+//  Created by Karsten Wennerlund on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct yourForestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
