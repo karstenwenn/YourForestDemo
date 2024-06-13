b33b2f3144fd650857b6b848eb63bb3f535a5aab
Prasiddha Shrestha <shrest5@stolaf.edu>
2024-06-13 14:50:35 -0500

    Second_push_prasiddha

--- yourForestDemo/experiment.swift
+++ yourForestDemo/experiment.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Create a button
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        button.setTitle("Tap Me", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        // Add button to the view
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonTapped() {
+        print("Button was tapped!")
+    }
+}
